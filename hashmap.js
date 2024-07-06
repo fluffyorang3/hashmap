c4889ef98cea0a677eeba0de8ad75a9bb1498cda
fluffyorang3 <fluffyorang3@gmail.com>
2024-07-06 15:21:14 +0200

    Implement has, remove and length function

--- hashmap.js
+++ hashmap.js
@@ -1,7 +1,6 @@
 function hashMap() {
-  let arrayLength = 16;
-  let array = new Array(arrayLength).fill(null);
-
+  let array_length = 16;
+  let array = new Array(array_length).fill(null);
   return {
     hash: function (key) {
       let hashCode = 0;
@@ -15,41 +14,87 @@ function hashMap() {
     },
     set: function (key, value) {
       let hashCode = this.hash(key);
-      let index = hashCode % arrayLength;
-
-      let newNode = { key, value, next: null };
+      let index = hashCode % array_length;
 
       if (array[index] === null) {
-        array[index] = newNode;
+        array[index] = { key, value, next: null };
       } else {
         let current = array[index];
-
-        while (current.next !== null) {
+        while (current) {
           if (current.key === key) {
             current.value = value;
             return;
           }
+          if (current.next === null) {
+            current.next = { key, value, next: null };
+            return;
+          }
           current = current.next;
-        }
-        if (current.key === key) {
-          current.value = value;
-        } else {
-          current.next = newNode;
         }
       }
     },
     get: function (key) {
       let hashCode = this.hash(key);
-      let index = hashCode % arrayLength;
-
+      let index = hashCode % array_length;
       let current = array[index];
-      while (current !== null) {
+
+      while (current) {
         if (current.key === key) {
           return current.value;
         }
         current = current.next;
       }
-      return undefined;
+      return "Key not found";
+    },
+    has: function (key) {
+      let hashCode = this.hash(key);
+      let index = hashCode % array_length;
+      let current = array[index];
+
+      while (current) {
+        if (current.key === key) {
+          return true;
+        }
+        current = current.next;
+      }
+      return false;
+    },
+    remove: function (key) {
+      let hashCode = this.hash(key);
+      let index = hashCode % array_length;
+      let current = array[index];
+      let previous = null;
+
+      while (current) {
+        if (current.key === key) {
+          if (previous === null) {
+            array[index] = current.next;
+          } else {
+            previous.next = current.next;
+          }
+          return true;
+        }
+        previous = current;
+        current = current.next;
+      }
+      return false;
+    },
+    length: function () {
+      let length = 0;
+
+      for (let i = 0; i < array_length; i++) {
+        let current = array[i];
+        if (current === null) {
+          continue;
+        }
+        while (current) {
+          if (current.key) {
+            length++;
+          }
+          current = current.next;
+        }
+      }
+      return length;
     },
   };
 }
